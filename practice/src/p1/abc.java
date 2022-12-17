4d3ace4f6861b69b10f553739eec4b379f8cf226
Ashish Khare <kharea970@gmail.com>
2022-12-18 02:07:14 +0530

    added database files

---
+++ practice/src/p1/abc.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class abc {
+
+}
+class A{
+    private int A;
+}
+class C extends A{
+
+}
---
+++ practice/src/p2/def.java
@@ -0,0 +1,17 @@
+package p2;
+import p1.*;
+interface ash {
+    public void act();
+}
+public class def {
+    public static void main(String args[]){
+        ash k = ()->{
+            System.out.println("dkjdkjdkjd");
+        };
+        k.act();
+    }
+}
+class E extends abc{
+
+}
+
